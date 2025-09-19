13d260c0427bc9e9d4273f4b1dcb0870a6742641
jhon <alecsey05car@gmail.com>
2025-09-19 16:21:14 -0500

    Implemetación de imagenes en el Header y diseño de interfazes del HEADER

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,42 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // <-- 1. IMPORTA RouterModule
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule], // <-- 2. AÑÁDELO A LOS IMPORTS
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
 export class HomeComponent {
   // Datos para las categorías de productos
   categories = [
-    { name: 'Procesadores', imageUrl: 'https://placehold.co/400x300/17162a/61d4b3?text=CPU' },
-    { name: 'Tarjetas Gráficas', imageUrl: 'https://placehold.co/400x300/17162a/61d4b3?text=GPU' },
-    { name: 'Motherboards', imageUrl: 'https://placehold.co/400x300/17162a/61d4b3?text=MOBO' },
-    { name: 'Memoria RAM', imageUrl: 'https://placehold.co/400x300/17162a/61d4b3?text=RAM' },
-    { name: 'Almacenamiento', imageUrl: 'https://placehold.co/400x300/17162a/61d4b3?text=SSD' },
-    { name: 'Periféricos', imageUrl: 'https://placehold.co/400x300/17162a/61d4b3?text=Teclados' }
+    { name: 'Procesadores', imageUrl: 'home-imgs/nuestras-categorias/CPU.png' },
+    { name: 'Tarjetas Gráficas', imageUrl: 'home-imgs/nuestras-categorias/GPU.png' },
+    { name: 'Motherboards', imageUrl: 'home-imgs/nuestras-categorias/MOTHERBOARD.png' },
+    { name: 'Memoria RAM', imageUrl: 'home-imgs/nuestras-categorias/RAM.png' },
+    { name: 'Almacenamiento', imageUrl: 'home-imgs/nuestras-categorias/ALMACENAMIENTO.png' },
+    { name: 'Chasis', imageUrl: 'home-imgs/nuestras-categorias/CHASIS.png' },
+    { name: 'Disipador', imageUrl: 'home-imgs/nuestras-categorias/DISIPADOR.png' },
+    { name: 'Ensambles', imageUrl: 'home-imgs/nuestras-categorias/ENSAMBLES.png' },
+    { name: 'Perifericos', imageUrl: 'home-imgs/nuestras-categorias/PERIFERICOS.png' },
+    { name: 'Software', imageUrl: 'home-imgs/nuestras-categorias/SOFTWARE.png' },
+    { name: 'Mantenimiento', imageUrl: 'home-imgs/nuestras-categorias/MANTENIMIENTO.png' }
+
   ];
 
   // Datos para productos destacados
   featuredProducts = [
     {
-      name: 'PC Gamer "Cyber-X"',
-      price: '$1,899.99',
-      imageUrl: 'https://placehold.co/600x400/17162a/f038ff?text=PC+Cyber-X',
+      name: 'Mause Razer "Viper V4 Pro"',
+      price: '$662.621,02',
+      imageUrl: 'home-imgs/productos-destacados/razer-viperv4.jpg',
       tag: 'Más Vendido'
     },
     {
-      name: 'GPU "Nova RTX 9090"',
-      price: '$1,200.00',
-      imageUrl: 'https://placehold.co/600x400/17162a/f038ff?text=RTX+9090',
+      name: 'GPU Gigabyte "AMD Radeon RX 9060Xt"',
+      price: '$1.861.643,09',
+      imageUrl: 'home-imgs/productos-destacados/RX9060xt.jpg',
       tag: 'Nuevo'
     },
     {
-      name: 'Teclado Mecánico "Quantum"',
-      price: '$159.99',
-      imageUrl: 'https://placehold.co/600x400/17162a/f038ff?text=Quantum+Keys',
+      name: 'Teclado Mecánico Redragon "DEIMOS K599"',
+      price: '$287.000',
+      imageUrl: 'home-imgs/productos-destacados/redragon-deimos.jpg',
       tag: 'Oferta'
     }
   ];
